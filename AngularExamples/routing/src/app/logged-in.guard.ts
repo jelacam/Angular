9dde3dac81bd8095a79f372e0aa835fd5da0b72f
Marko Jelača <jelaca.marko@gmail.com>
2017-06-10 00:58:52 +0200

    Dodatak.

--- AngularExamples/routing/src/app/logged-in.guard.ts
+++ AngularExamples/routing/src/app/logged-in.guard.ts
@@ -5,12 +5,14 @@ import { AuthService } from './services/auth.service'
 
 @Injectable()//This must be here, if we want to inject authService!
 export class LoggedInGuard implements CanActivate{
-    
+
+    // prosledjen autenitfikacioni servis
     constructor(private authService: AuthService){
     }
 
+    /// metoda vraca boolean ili Observable ili Promise
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
-        return this.authService.isLoggedIn();
+        return this.authService.isLoggedIn(); // vrsi proveru da li je logovan i vraca true (sme da ode na protecte) ili false (ne sme da ode na protected)
     }
 
 
